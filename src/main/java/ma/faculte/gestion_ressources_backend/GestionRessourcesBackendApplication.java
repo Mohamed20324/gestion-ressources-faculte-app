6b0c0d93f92ac521ddd5d48e406fb132415eff90
Mohamed EL Mardi <94396762+Mohamed20324@users.noreply.github.com>
2026-04-09 16:05:38 +0100

    Initial commit

---
+++ src/main/java/ma/faculte/gestion_ressources_backend/GestionRessourcesBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.faculte.gestion_ressources_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRessourcesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRessourcesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/faculte/gestion_ressources_backend/GestionRessourcesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.faculte.gestion_ressources_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRessourcesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
